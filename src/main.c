b2bfc1ffb6692ff86841677386e84cd9e80d4972
Yauheni Vasileuski <jonnyenzo@gmail.com>
2018-09-20 22:14:54 +0000

    Preparing

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    //TODO 
+    include spec lib for img
+    
+    Open file
+        Chose type of file
+    Use it
+        Iteration by horizontal
+            Iteration by vertical
+                Apply alghorithm
+    Close file
+    return 0;
+}
